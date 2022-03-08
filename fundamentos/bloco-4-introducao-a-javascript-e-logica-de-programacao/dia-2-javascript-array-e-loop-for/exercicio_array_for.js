43c8b50287ed7b4589eadaad9acf166e36ef5057
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-08 18:02:20 -0300

    Exercício 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_array_for.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_array_for.js
@@ -2,8 +2,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (index of numbers){
-    if (index > soma){
-        soma = index;
+    if (index % 2 !==0){
+        soma += 1;
     }
 }
-console.log(soma);
+if (soma > 0){
+    console.log(soma);
+}
+else {
+    console.log('nenhum valor ímpar encontrado');
+}
