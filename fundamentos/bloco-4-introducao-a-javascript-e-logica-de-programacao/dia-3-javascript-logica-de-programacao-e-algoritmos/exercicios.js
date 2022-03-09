758fcf81361f6cd0fc935d0bca42edb15e63e6cf
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-09 15:42:27 -0300

    Exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,4 +1,4 @@
-//Ex 1
+/*Ex 1
 
 let number = 4;
 let fatorial = 1;
@@ -7,3 +7,14 @@ for (let i = number; i >= 1; i -= 1 ){
     fatorial = fatorial * i;
     console.log(fatorial);
 }
+*/
+
+//Ex 2
+
+let word = 'banana';
+let palavraInvert = '';
+
+for (let i = word.length - 1; i >=0; i -=1){
+    palavraInvert = palavraInvert + word[i];
+}
+console.log(palavraInvert);
