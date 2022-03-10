beedc339e49a47b0c3f5bb346f3db05521d98a4b
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-10 18:29:28 -0300

    Parte 1 - Exercicio 6

--- fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
+++ fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
@@ -8,7 +8,7 @@ let info = {
   console.log('Bem-vinda, '+ info.personagem);
   */
 
-  //Ex 2 e 3
+  /*Ex 2 e 3
 
   let info = {
     personagem: 'Margarida',
@@ -31,7 +31,7 @@ let info = {
   //console.log(info[i]);
  //}
 
- //Ex 5
+ /*Ex 5
 
  for (let i in info){
      if (i === 'recorrente'){
@@ -41,4 +41,21 @@ let info = {
         console.log(info[i] + ' e '+ info2[i]);
      }
  }
+*/
 
+//Ex 6
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log('O livro favorito de '+ leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
