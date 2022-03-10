359101263e5faf7b136a436823d5ccfeefd2cf54
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-10 15:41:19 -0300

    Exercícios finalizados

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -41,7 +41,7 @@ for (let i of array){
 console.log('A menor palavra é: ' + menorPalavra);
 */
 
-//Ex 4
+/*Ex 4
 
 let ultimoNumero = 50;
 let maiorPrimo = 0;
@@ -59,5 +59,19 @@ for (i = 2; i <= ultimoNumero ; i += 1){
     count = 0;  
 }
 console.log(maiorPrimo);
+*/
+const n = 15;
+const numeroDeLinhas = (n + 1) / 2;
+let asteriscos = '*';
+let espaços = '';
+let aux = numeroDeLinhas - 1;
 
-
+for (let i = 1; i <= n; i += 2) {
+    for (let z = aux; z > 0; z -= 1) {
+        espaços += ' ';
+    }
+    console.log(espaços + asteriscos);
+    asteriscos += '**';
+    espaços = '';
+    aux -= 1;
+}
