2541dd83ae6351db1c2246c69030b96fe6283f29
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-10 15:57:03 -0300

    Parte 1 - Exercicios 2 e 3

--- fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
+++ fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
@@ -10,11 +10,13 @@ let info = {
 
   //Ex 2
 
-  {
+  let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
   }
 
-  
+  for (let i in info){
+      console.log(i);
+  }
