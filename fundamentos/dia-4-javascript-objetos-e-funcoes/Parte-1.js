0f846694d6bb175e7fb35ec6ad7dcc3f0462dce0
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-10 18:19:11 -0300

    Parte 1 - Exercicio 5

--- fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
+++ fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-1.js
@@ -16,11 +16,29 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
   }
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  }
 
- for (let i in info){
+// for (let i in info){
 //Ex 3   console.log(i);
 // }
 
   //Ex 4 
   //console.log(info[i]);
  //}
+
+ //Ex 5
+
+ for (let i in info){
+     if (i === 'recorrente'){
+         console.log("Ambos recorrentes")
+     }
+     else {
+        console.log(info[i] + ' e '+ info2[i]);
+     }
+ }
+
