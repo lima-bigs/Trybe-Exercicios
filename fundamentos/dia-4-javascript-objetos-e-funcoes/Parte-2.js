ed23ed54601fc0fa5a385008b47c65194513e0ce
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-11 11:55:47 -0300

    Parte 2 - Exercicio 4

--- fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-2.js
+++ fundamentos/dia-4-javascript-objetos-e-funcoes/Parte-2.js
@@ -31,7 +31,7 @@ console.log(indexHighestValue([2, 3, 6, 7, 10, 1]));
 
 //*/
 
-//Ex 3
+/*Ex 3
 
 function indexLowestValue (array = []){
     let lowestValue = array [0];
@@ -46,3 +46,17 @@ function indexLowestValue (array = []){
 console.log(indexLowestValue([2, 4, 6, 7, 10, 0, -3]));
 
 //*/
+
+//Ex 4
+
+function bigestName (array = ['']){
+    let name = array [0];
+    for (let index of array){
+        if (index.length > name.length){
+            name = index;
+        }
+    }
+    return name;
+}
+
+console.log(bigestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
