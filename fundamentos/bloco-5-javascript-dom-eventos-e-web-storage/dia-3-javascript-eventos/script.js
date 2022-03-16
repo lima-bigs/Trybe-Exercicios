3121236eb54308f765aff7de8421d3e962da797b
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-16 16:22:02 -0300

    Concluido o exrcicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,30 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Ex 1
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const listaDias = document.querySelector('days');
+  for (let index of dezDaysList){
+    let dia = document.createElement('li');
+    dia.innerText = index;
+    dia.classList.add('day');
+    if (index === 24 || index === 25 || index === 31){
+        dia.classList.add('holiday');
+    }
+    if (index === 4 || index === 11 || index === 18 || index === 25 ){
+        dia.classList.add('friday');
+    }
+    listaDias.appendChild(dia);
+  }
