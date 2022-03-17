3b63a257304a288e2c1a11aebbcc54cf48296e98
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-17 15:49:37 -0300

    Corrigindo o exercicio 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,61 +1,63 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  createDaysOfTheWeek();
-  
-  // Ex 1
 
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  const listaDias = document.querySelector('#days');
-  for (let index of dezDaysList){
+    for (let index = 0; index < weekDays.length; index += 1) {
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+
+createDaysOfTheWeek();
+
+// Ex 1
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const listaDias = document.querySelector('#days');
+for (let index of dezDaysList) {
     let dia = document.createElement('li');
     dia.innerText = index;
     dia.classList.add('day');
-    if (index === 24 || index === 25 || index === 31){
+    if (index === 24 || index === 25 || index === 31) {
         dia.classList.add('holiday');
     }
-    if (index === 4 || index === 11 || index === 18 || index === 25 ){
+    if (index === 4 || index === 11 || index === 18 || index === 25) {
         dia.classList.add('friday');
     }
     listaDias.appendChild(dia);
-  }
+}
 
-  // Ex 2
+// Ex 2
 
-  function criaBotao(nomeBotao){
+function criaBotao(nomeBotao) {
     let auxbotao = document.querySelector('.buttons-container');
     let botao = document.createElement('button');
     botao.id = 'btn-holiday';
     botao.innerHTML = nomeBotao;
     auxbotao.appendChild(botao);
-  }
+}
 
-  // Ex 3
+criaBotao('Feriados');
 
-  function clickFeriadoCor(){
-      let botao = document.querySelector('#btn-holiday');
-      let diasFeriados = document.getElementsByClassName('holiday');
-      let corOriginal = 'rgb(238,238,238)';
-      let novaCor = 'yellow';
+// Ex 3
 
-      botao.addEventListener('click', function(){
-        if (diasFeriados.style.background == corOriginal){
-            diasFeriados.style.background = novaCor;    
+function clickFeriadoCor() {
+    let botao = document.querySelector('#btn-holiday');
+    let diasFeriados = document.getElementsByClassName('holiday');
+
+    botao.addEventListener('click', function () {
+        for (let index = 0; index < diasFeriados.length; index += 1) {
+            if (diasFeriados[index].style.backgroundColor === '') {
+                diasFeriados[index].style.backgroundColor = 'yellow';
+            }
+            else if (diasFeriados[index].style.backgroundColor === 'yellow') {
+                diasFeriados[index].style.backgroundColor = '';
+            }
         }
-        else {
-            diasFeriados.style.background = corOriginal;
-        }
-      });
-  };
+    });
+};
 
-  clickFeriadoCor();
+clickFeriadoCor();
