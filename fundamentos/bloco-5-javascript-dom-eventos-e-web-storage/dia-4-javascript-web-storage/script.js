d34e960d8b9aab5db5f0b93798734e9aa9050421
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-18 13:30:06 -0300

    Implentados botoes de cor de fonte

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -13,7 +13,9 @@ function fundoVerde() {
     let botao = document.getElementsByClassName('btn-cor-fundo verde')[0];
     let fundo = document.getElementsByClassName('texto')[0];
     botao.addEventListener('click', function () {
-        fundo.style.background = 'lightgreen';
+        let escolha = 'lightgreen';
+        fundo.style.background = escolha;
+        localStorage.setItem('Cor-Fundo', escolha);
     })
 };
 fundoVerde();
@@ -22,8 +24,42 @@ function fundoBranco() {
     let botao = document.getElementsByClassName('btn-cor-fundo branco')[0];
     let fundo = document.getElementsByClassName('texto')[0];
     botao.addEventListener('click', function () {
-        fundo.style.background = 'white';
+        let escolha = 'white';
+        fundo.style.background = escolha;
+        localStorage.setItem('Cor-Fundo', escolha);
     })
 };
 fundoBranco();
 
+function fontePreto() {
+    let botao = document.getElementsByClassName('btn-cor-fonte preto')[0];
+    let fundo = document.getElementsByClassName('texto')[0];
+    botao.addEventListener('click', function () {
+        let escolha = 'black';
+        fundo.style.color = escolha;
+        localStorage.setItem('Cor-Fonte', escolha);
+    })
+};
+fontePreto();
+
+function fonteVerde() {
+    let botao = document.getElementsByClassName('btn-cor-fonte verde')[0];
+    let fundo = document.getElementsByClassName('texto')[0];
+    botao.addEventListener('click', function () {
+        let escolha = 'green';
+        fundo.style.color = escolha;
+        localStorage.setItem('Cor-Fonte', escolha);
+    })
+};
+fonteVerde();
+
+function fonteCinza() {
+    let botao = document.getElementsByClassName('btn-cor-fonte cinza')[0];
+    let fundo = document.getElementsByClassName('texto')[0];
+    botao.addEventListener('click', function () {
+        let escolha = 'gray';
+        fundo.style.color = escolha;
+        localStorage.setItem('Cor-Fonte', escolha);
+    })
+};
+fonteCinza();
