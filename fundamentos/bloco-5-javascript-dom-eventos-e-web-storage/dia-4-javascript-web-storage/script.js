afd72d35889c9e854bf60aa85926acb412b79408
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-03-18 13:18:24 -0300

    Implentados botoes de cor de fundo

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,9 +1,29 @@
-function mudaCorFundo() {
+function fundoPreto() {
     let botao = document.getElementsByClassName('btn-cor-fundo preto')[0];
     let fundo = document.getElementsByClassName('texto')[0];
-    botao.addEventlistener('click', function () {
-        fundo.style.background = 'black';
-        console.log('oi');
+    botao.addEventListener('click', function () {
+        let escolha = 'black';
+        fundo.style.background = escolha;
+        localStorage.setItem('Cor-Fundo', escolha);
     })
 };
-mudaCorFundo();
+fundoPreto();
+
+function fundoVerde() {
+    let botao = document.getElementsByClassName('btn-cor-fundo verde')[0];
+    let fundo = document.getElementsByClassName('texto')[0];
+    botao.addEventListener('click', function () {
+        fundo.style.background = 'lightgreen';
+    })
+};
+fundoVerde();
+
+function fundoBranco() {
+    let botao = document.getElementsByClassName('btn-cor-fundo branco')[0];
+    let fundo = document.getElementsByClassName('texto')[0];
+    botao.addEventListener('click', function () {
+        fundo.style.background = 'white';
+    })
+};
+fundoBranco();
+
