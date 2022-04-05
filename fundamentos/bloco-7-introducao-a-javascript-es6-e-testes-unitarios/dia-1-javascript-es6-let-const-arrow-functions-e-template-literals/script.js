3c3eba1faeee5eb07d32939e4445793a43987892
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-05 12:23:01 -0300

    Realizado o exercicio 1 da parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -14,20 +14,20 @@
 
 // Ex 1
 
-  const testingScope = (escopo) => {
-    let ifScope;
-    if (escopo === true) {
-      ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+  // const testingScope = (escopo) => {
+  //   let ifScope;
+  //   if (escopo === true) {
+  //     ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+  //     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+  //     console.log(ifScope);
+  //   } else {
+  //     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+  //     console.log(elseScope);
+  //   }
+  //   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  // }
 
-  testingScope(true);
+  // testingScope(true);
 
   // Ex 2
 
@@ -49,6 +49,23 @@
 
 // Ex bônus
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-oddsAndEvens.sort((a, b) => a - b);
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// oddsAndEvens.sort((a, b) => a - b);
+// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+// Parte 2
+// Ex 1
+
+// const fatorial = (numero) => {
+//   let result = 1;
+//   for ( let i = 1; i <= numero; i += 1){
+//     result *= i;
+//   }
+//   return result;
+// }
+// console.log(fatorial(3));
+
+// // Ex 1 bônus
+
+// const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+// console.log(factorial(5));
