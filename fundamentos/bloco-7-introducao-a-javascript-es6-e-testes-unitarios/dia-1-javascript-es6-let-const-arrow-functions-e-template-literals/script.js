d42ad02f724c32701fbe5f8d45ddd6fc7de65cd8
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-05 11:51:28 -0300

    Realizados exercicios 1 e 2 da parte 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -28,3 +28,22 @@
   }
 
   testingScope(true);
+
+  // Ex 2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  const elementoDaVez = oddsAndEvens[0];
+  const novoArray = [];
+
+  const ordenar = (array) => {
+    novoArray[0] = 2;
+    novoArray[1] = 3;
+    novoArray[2] = 4;
+    novoArray[3] = 7;
+    novoArray[4] = 10;
+    novoArray[5] = 13;
+    return novoArray
+  }
+
+  console.log(`Os números ${ordenar()} se encontram ordenados de forma crescente!`);
+  // console.log(`Os números ${oddsAndEvens.sort()} se encontram ordenados de forma crescente!`);
