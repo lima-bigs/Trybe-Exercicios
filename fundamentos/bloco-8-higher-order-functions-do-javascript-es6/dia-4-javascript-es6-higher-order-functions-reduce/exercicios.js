1802c7b8180446048c8541da675ddcc2bcca5c9a
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-12 20:16:53 -0300

    Inciciado exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
@@ -77,8 +77,50 @@ const books = [
   
 // // Ex 2
 
-const reduceNames = books.reduce((acumulador, elemento, index, array)=> {
-    return index < array.length -1 ? acumulador + `${elemento.author.name}, `: acumulador + `${elemento.author.name}.`;
-},'');
-console.log(reduceNames);
+// const reduceNames = books.reduce((acumulador, elemento, index, array)=> {
+//     return index < array.length -1 ? acumulador + `${elemento.author.name}, `: acumulador + `${elemento.author.name}.`;
+// },'');
+// console.log(reduceNames);
 
+// Ex 3
+
+// const mediaIdade = books.reduce((acc, livro, index, array)=>{
+//     let idade = (livro.releaseYear - livro.author.birthYear);
+//     acc += idade;
+//     return acc
+// }, 0);
+
+// console.log(mediaIdade/books.length);
+
+// Ex 4
+
+// const maiorNome = books.reduce((acc, livro)=> {
+//     return (acc.name.length > livro.name.length) ? acc : livro;
+// });
+
+// console.log(maiorNome);
+
+// Ex 5
+
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  function containsA() {
+    names.reduce((acc, nome)=> {
+        let nomesSplitados = nome.split('')
+        console.log(nomesSplitados);
+        nomesSplitados.reduce((acc, letra)=>{
+            console.log(letra);
+            if(letra === 'a' || letra === 'A'){
+                acc += 1;
+                console.log(acc);
+            };
+            return acc
+        },0);
+    }, ''); 
+    // return acc;
+  };
+  console.log(containsA());
