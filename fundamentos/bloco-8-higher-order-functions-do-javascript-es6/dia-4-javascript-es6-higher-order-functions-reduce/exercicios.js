2c06959a7dcd6b485a8d00856673baed08ea4a8d
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-12 15:31:00 -0300

    Concluido o exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
@@ -1,13 +1,84 @@
-// Ex 1
+// // Ex 1
 
-const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
+// const arrays = [
+//     ['1', '2', '3'],
+//     [true],
+//     [4, 5, 6],
+//   ];
+  
+//   function flatten() {
+//     return arrays.reduce((novoArray, elemento) => novoArray.concat(elemento));
+//   }
+
+//   console.log(flatten(arrays));
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
   ];
   
-  function flatten() {
-    return arrays.reduce((novoArray, elemento) => novoArray.concat(elemento));
-  }
+// // Ex 2
 
-  console.log(flatten(arrays));
+const reduceNames = books.reduce((acumulador, elemento)=> {
+    acumulador + `${elemento.author.name}, `,'';
+    return
+})
+console.log(reduceNames);
