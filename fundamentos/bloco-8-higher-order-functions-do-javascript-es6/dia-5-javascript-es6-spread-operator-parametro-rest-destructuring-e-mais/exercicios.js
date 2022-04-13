2bcf97e9c66e933b5280a9abc3c51ce73292a3b0
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-13 20:41:04 -0300

    Concluido o exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
@@ -22,53 +22,58 @@
 
 // Ex 3 ??????
 
-const alex = {
-    name: 'Alex',
-    age: 26,
-    likes: ['fly fishing'],
-    nationality: 'Australian',
-};
+// const alex = {
+//     name: 'Alex',
+//     age: 26,
+//     likes: ['fly fishing'],
+//     nationality: 'Australian',
+// };
 
-const gunnar = {
-    name: 'Gunnar',
-    age: 30,
-    likes: ['hiking', 'scuba diving', 'taking pictures'],
-    nationality: 'Icelandic',
-};
+// const gunnar = {
+//     name: 'Gunnar',
+//     age: 30,
+//     likes: ['hiking', 'scuba diving', 'taking pictures'],
+//     nationality: 'Icelandic',
+// };
 
-const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-console.log(personLikes(alex));
-console.log(personLikes(gunnar));
+// console.log(personLikes(alex));
+// console.log(personLikes(gunnar));
 
 // Ex 4
 
-// const people = [
-//     {
-//       name: 'Nicole',
-//       bornIn: 1992,
-//       nationality: 'Australian',
-//     },
-//     {
-//       name: 'Harry',
-//       bornIn: 2008,
-//       nationality: 'Australian',
-//     },
-//     {
-//       name: 'Toby',
-//       bornIn: 1901,
-//       nationality: 'Australian',
-//     },
-//     {
-//       name: 'Frida',
-//       bornIn: 1960,
-//       nationality: 'Dannish',
-//     },
-//     {
-//       name: 'Fernando',
-//       bornIn: 2001,
-//       nationality: 'Brazilian',
-//     },
-//   ];
-  
-//   // escreva filterPeople abaixo
+const people = [
+    {
+        name: 'Nicole',
+        bornIn: 1992,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Harry',
+        bornIn: 2008,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Toby',
+        bornIn: 1901,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Frida',
+        bornIn: 1960,
+        nationality: 'Dannish',
+    },
+    {
+        name: 'Fernando',
+        bornIn: 2001,
+        nationality: 'Brazilian',
+    },
+];
+
+const selecionaPessoas = (pessoas) => 
+    pessoas.filter(({ bornIn, nationality }) => bornIn > 1900 && bornIn < 2001 && nationality === 'Australian');
+;
+console.log(selecionaPessoas(people));
+
+  // escreva filterPeople abaixo
