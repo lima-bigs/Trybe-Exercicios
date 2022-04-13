4c632da6aa5cbc0c0b3cb4c7359244295c3ce425
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-13 18:15:37 -0300

    Concluidos os exercicios 1 e 2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
@@ -0,0 +1,20 @@
+// Ex 1
+
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+//   rectangleArea(...rectangle) // altere a chamada da funcao rectangleArea
+//   console.log(rectangle[0] * rectangle[1]);
+// });
+
+// Ex 2
+
+const sum = (...param) => param.reduce((acc, cur)=> acc + cur);
+
+console.log(sum(1,2,3,4,5,6));
+
