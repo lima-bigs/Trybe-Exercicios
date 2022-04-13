20bdc63d97676f6523f276efc411947c172985af
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-13 18:54:43 -0300

    Concluido o exercicio 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
@@ -14,7 +14,61 @@
 
 // Ex 2
 
-const sum = (...param) => param.reduce((acc, cur)=> acc + cur);
+// const sum = (...param) => param.reduce((acc, cur)=> acc + cur);
+// console.log(sum(1,2,3,4,5,6));
 
-console.log(sum(1,2,3,4,5,6));
+// const sum = (param) => param.reduce((acc, cur)=> acc + cur);
+// console.log(sum([1,2,3,4,5,6]));
 
+// Ex 3 ??????
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+};
+
+const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+};
+
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
+
+// Ex 4
+
+// const people = [
+//     {
+//       name: 'Nicole',
+//       bornIn: 1992,
+//       nationality: 'Australian',
+//     },
+//     {
+//       name: 'Harry',
+//       bornIn: 2008,
+//       nationality: 'Australian',
+//     },
+//     {
+//       name: 'Toby',
+//       bornIn: 1901,
+//       nationality: 'Australian',
+//     },
+//     {
+//       name: 'Frida',
+//       bornIn: 1960,
+//       nationality: 'Dannish',
+//     },
+//     {
+//       name: 'Fernando',
+//       bornIn: 2001,
+//       nationality: 'Brazilian',
+//     },
+//   ];
+  
+//   // escreva filterPeople abaixo
