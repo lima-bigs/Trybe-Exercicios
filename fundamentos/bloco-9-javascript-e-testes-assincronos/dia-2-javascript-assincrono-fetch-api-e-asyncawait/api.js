d73911c3136c2a781265958e76b0bc003ebc179a
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-04-25 17:24:31 -0300

    Iniciando exercicio 2

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,12 @@
+const { Response } = require("node-fetch");
+
+// apiScript.js
+
+const fetchCoin = async () => {
+    const API_URL = `https://api.coincap.io/v2/assets`;
+  await fetch(API_URL)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+fetchCoin();
--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -1,8 +1,16 @@
+const { Response } = require("node-fetch");
+
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+      method : 'GET',
+      headers : { 'Accept': 'application/json'},
+  }
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => console.log(data));
 };
 
-window.onload = () => fetchJoke();
+window.onload = fetchJoke();
